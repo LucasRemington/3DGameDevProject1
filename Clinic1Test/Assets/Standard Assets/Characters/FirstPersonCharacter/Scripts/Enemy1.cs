d5fa79ccf67681c84b7e5105d4f225bd181bac91
FSCAD\lreming1 <lreming1@student.fitchburgstate.edu>
2018-02-01 15:02:28 -0500

    Bunny death

--- Clinic1Test/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Enemy1.cs
+++ Clinic1Test/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Enemy1.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 public class Enemy1 : MonoBehaviour {
 
     public int currentHP1 = 1;
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
 
     public void TakeDamage( int damageAmount)
+
+
     {
         currentHP1 -= damageAmount;
         if (currentHP1 <= 0)
         {
 			GameController.killScore++;
-			Destroy(gameObject);
+			anim.SetTrigger ("death");
+			Destroy(gameObject, 0.5f);
         }
     }
+
 }
