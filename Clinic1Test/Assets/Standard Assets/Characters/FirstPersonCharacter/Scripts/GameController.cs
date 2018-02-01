bf1307bcf00d2935791ddf11a8e9f2c3436364cd
FSCAD\lreming1 <lreming1@student.fitchburgstate.edu>
2018-02-01 13:36:34 -0500

    Added UI + first animations

--- Clinic1Test/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/GameController.cs
+++ Clinic1Test/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/GameController.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public GameObject[] enemies;
 	public GameObject gates;
-	public int gateHP = 10;
+	public static int gateHP = 10;
 	public int maxWaves = 10;
 	public int currentWave = 0;
 	public float spawnTime = 3f;
 	public Transform[] spawnPoints;
 	private WaitForSeconds waveDuration = new WaitForSeconds(2.5f);
+	public static int killScore = 0;
+	public Text gateHealthcount;
+	public Text killScorecount;
+	public Text waveCount;
 
 	void Start () 
 	{
@@ -21,7 +26,9 @@ public class GameController : MonoBehaviour {
 
 	void Update()
 	{
-		
+		gateHealthcount.text = "Health: " + gateHP.ToString ();
+		waveCount.text = "Wave " + currentWave.ToString ();
+		killScorecount.text = "Score: " + killScore.ToString ();
 	}
 
 	private IEnumerator WaveMode()
